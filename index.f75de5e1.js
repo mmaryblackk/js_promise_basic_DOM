584341fab9396ef874737ecae05d5c493e08e5eb
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-05-22 22:51:48 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+/* eslint-disable promise/param-names */ /* eslint-disable prefer-promise-reject-errors */ "use strict";
+const logo = document.querySelector(".logo");
+const promise1 = new Promise((resolve)=>{
+    logo.addEventListener("click", ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((_, reject)=>{
+    setTimeout(()=>{
+        reject();
+    }, 3000);
+});
+const handleSuccess = ()=>{
+    const message = document.createElement("div");
+    message.className = "message";
+    message.textContent = "Promise was resolved!";
+    document.body.appendChild(message);
+};
+const handleError = ()=>{
+    const message = document.createElement("div");
+    message.className = "message error-message";
+    message.textContent = "Promise was rejected!";
+    document.body.appendChild(message);
+};
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
+
+//# sourceMappingURL=index.f75de5e1.js.map
