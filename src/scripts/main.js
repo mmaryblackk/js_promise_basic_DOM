bf8043ea721a125074d7ac041230707ca65d4890
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-05-22 22:51:34 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
+/* eslint-disable promise/param-names */
+/* eslint-disable prefer-promise-reject-errors */
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((_, reject) => {
+  setTimeout(() => {
+    reject();
+  }, 3000);
+});
+
+const handleSuccess = () => {
+  const message = document.createElement('div');
+
+  message.className = 'message';
+  message.textContent = 'Promise was resolved!';
+  document.body.appendChild(message);
+};
+
+const handleError = () => {
+  const message = document.createElement('div');
+
+  message.className = 'message error-message';
+  message.textContent = 'Promise was rejected!';
+  document.body.appendChild(message);
+};
+
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
